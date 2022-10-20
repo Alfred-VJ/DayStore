a04dc4027a91fa5bfc0c5302c953cb458349ec43
Alfredo Velázquez <alfredo.velazquez@transgenia.org>
2022-10-19 19:26:29 -0500

    first-sage

--- app/functions/index.js
+++ app/functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const serviceAccount = require('../permisos.json')
+const serviceAccount = require('../key.json')
 
 const app = express();
 app.use(cors({ origin: true }));
